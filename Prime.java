4b742ee4959e21eba65183f7c50b370ab807c63d
somaharini <somaharini24@gmail.com>
2025-01-27 18:56:16 +0530

    Add files via upload

--- Prime.java
+++ Prime.java
@@ -1,12 +1,23 @@
-import java.util.scanner;
-public class Prime;
-
-
-
-
-
-
-
-
-
-
+import java.util.Scanner;
+public class Prime{
+    public static boolean isprime(int num){
+        if(num<=1){
+            return false;
+        }
+        for (int i=2; i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+    return true;
+}
+public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    int num =scanner.nextInt();
+    if(isprime(num)){
+        System.out.println("true");
+    }else{
+        System.out.println("false");
+    }
+    }
+    }
